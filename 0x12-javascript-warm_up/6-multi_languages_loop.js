c0868c5ef2ffc1d14f75f0bea3aae48c8a44ff91
Kenny15200 <opaleyekehinde70@gmail.com>
2023-05-16 20:54:47 +0100

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//script that prints 3 lines
+//@: using while loop to iterate the 3 lines
+let multiLoop = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < multiLoop.length) {
+        console.log(multiLoop[i]);
+        i++;
+}
