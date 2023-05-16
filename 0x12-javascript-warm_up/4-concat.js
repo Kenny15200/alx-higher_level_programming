1cf7ca81544b36835c7d8e304b799cdcacd68202
Kenny15200 <opaleyekehinde70@gmail.com>
2023-05-16 15:35:12 +0100

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-//Write a script that prints two arguments passed
+// Write a script that prints two arguments passed
 //
 
 if (process.argv[2] && process.argv[3]) {
-        console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else if (process.argv[2] && process.argv[3] === undefined)  {
-        console.log(process.argv[2] + ' is ' + process.argv[3]);
-}else {
-        console.log('undefined is undefined');
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (process.argv[2] && process.argv[3] === undefined) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else {
+  console.log('undefined is undefined');
 }
