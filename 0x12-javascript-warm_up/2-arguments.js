e7b79f1ddf1a2269a55c336fea9ea6aec9f07df1
Kenny15200 <opaleyekehinde70@gmail.com>
2023-05-16 11:04:00 +0100

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,10 +4,10 @@
 // @b: if "Argument found"
 // @c: otherwise "Arguments found"
 
-const nArgs = process.argv.length - 2;
-if (nArgs === 0) {
-  console.log('No arguments passed');
-} else if (nArgs === 1) {
+const nArgs = process.argv.slice(2);
+if (nArgs.length === 0) {
+  console.log('No arguments');
+} else if (nArgs.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
