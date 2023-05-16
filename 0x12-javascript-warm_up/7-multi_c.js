0369f4e0215c06ce9641e281329506d179a31ffe
Kenny15200 <opaleyekehinde70@gmail.com>
2023-05-16 23:38:22 +0100

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 // a script that prints x times “C is fun”
-if (process.argv.length < 3) {
-  console.log('Missing number of occurrences');
-} else {
-  const x = parseInt(process.argv[2]);
-  if (isNaN(x)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < x; i++) {
+const x = parseInt(process.argv[2]);
+if (isNaN(x)) {
+	console.log('Missing number of occurrences'); }
+else {
+	for (let i = 0; i < x; i++) {
       console.log('C is fun');
     }
   }
-}
