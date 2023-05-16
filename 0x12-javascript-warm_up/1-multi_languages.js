358cce6971c15ac1ba290589cca6b09e66cbd8b6
Kenny15200 <opaleyekehinde70@gmail.com>
2023-05-16 09:55:25 +0100

    3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A script that prints 3 lines
+// @ first line: C is Fun
+// @ Second line: Python is cool
+// @ Third line: JavaScript is amazing 
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
