2ec28a067ce0e3549478b7f092ab1fe4dc71dd74
Kenny15200 <opaleyekehinde70@gmail.com>
2023-05-19 18:32:58 +0100

    Add file

---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/* a function that returns the addition of 2 integers.
+ *
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+*/
+
+function add (a, b) {
+  return a + b;
+}
+module.exports = {
+  add: add
+};
